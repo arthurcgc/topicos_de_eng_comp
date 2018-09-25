a55a4a6b7d3d9088a55c08516b3f112a2679a6fe
arthurcgc <arthur.cavalcante.puc@gmail.com>
2018-09-25 16:45:23 -0300

    ta saindo da jaula o RSSreader!

--- rssfeeder/RSSfeeder.py
+++ rssfeeder/RSSfeeder.py
@@ -1,4 +1,6 @@
 import feedparser as fp
+import re
+
 
 def ask_urls():
     urls =[]
@@ -36,11 +38,41 @@ def display_articles(feed):
     print(len(feed.entries))
     for i in range(len(feed.entries)):
         print("Article {}".format(i+1))
-        print("\t\tTitle\t\tAuthor\t\tCategory\t\tPublished\t\t")
-        print()
-        print("\t\t{0}\t\t{1}\t\t{2}\t\t{3}\t\t".format(feed.entries[i].title, feed.entries[i].source.title, feed.entries[i].category, feed.entries[i].published))
-            
-    
+        try:
+            title = feed.entries[i].title
+        except:
+            title = "Unknown"
+        try:
+            author = feed.entries[i].source.title
+        except:
+            try:
+                author = feed.entries[i].author
+            except:
+                author = "Unknown"
+        try:
+            category = feed.entries[i].category
+        except:
+            category = "Unknown"
+        try:
+            date = feed.entries[i].published
+        except:
+            date = "Unknown"
+        print("Title: {}\nAuthor: {}\nCategory: {}\nDate Published: {}\n".format(title, author, category, date))
+
+def display_description(feed_entry):
+    summary = feed_entry.summary
+    remove_tag = re.compile(r"(<.*>)([^.]*)")
+    match = remove_tag.findall(summary)
+    #print(match[0][1])
+    #import pdb;pdb.set_trace()
+    if match[0][1] != None:
+        print(match[0][1] + "...")
+    else:
+        print(match[0][0] + "...")
+    ask_summary = input("Do you wish to display the whole text of the article? [Y/N]\n")
+    if ask_summary == "Y":
+        print(summary)
+
 
 if __name__=="__main__":
     urls = ask_urls()
@@ -57,4 +89,14 @@ if __name__=="__main__":
     index = int(input("From the list above, type the number that corresponds to the url you wish to see the articles from\n"))
     feed = fp.parse(urls[index-1])
     display_articles(feed)
+    while True:
+        q_read_article = input("Do you wish to see a description from an article from the list above? Y/N\n")
+        if q_read_article == "Y":
+            art_index = int(input("Type the article number you wish to see:\n")) - 1
+            display_description(feed.entries[art_index])
+        elif q_read_article == "N":
+            break
+        else:
+            pass
+
 
